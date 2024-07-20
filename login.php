8ffb4724eba7369da09e3d2b862942df291e8fd4
AdemarLira <ademarliraneto@gmail.com>
2024-07-19 21:34:59 -0300

    ajustes

--- login.php
+++ login.php
@@ -8,8 +8,8 @@ include_once('conexao.php');
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styleLogin.css">
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="styleLogin.css">
   <title>TASK List</title>
 </head>
 
