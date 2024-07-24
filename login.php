f2b4fc939c6c8d1f8166f75da3de2b63178373c4
AdemarLira <ademarliraneto@gmail.com>
2024-07-24 12:10:18 -0300

    organizando as pastas

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-include_once('conexao.php');
+include_once("conexao.php");
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +9,7 @@ include_once('conexao.php');
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="styleLogin.css">
+  <link rel="stylesheet" href="css/styleLogin.css">
   <title>TASK List</title>
 </head>
 
